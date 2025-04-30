c89954ebe55e7b7b7aafecbbe69ce559fc91693e
carriegale2710 <carriegale2710@gmail.com>
2025-04-30 10:26:35 +1000

    creates BookCard component files and dummy test data bookData json file

---
+++ g-books-app/src/components/BookCard/BookCard.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import Card from './Card';
+
+describe('Card', () => {
+//   it('Should render', () => {
+//     render(<Card title="Test card" content="some content" />);
+//     const article = screen.getByRole('article');
+//     // console.log(article);
+//     expect(article).toBeInTheDocument();
+//   });
+
+    
+
+  
+});
